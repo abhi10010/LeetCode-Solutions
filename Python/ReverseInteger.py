01426d38ce274f6a3182f01e95af69157c113730
Abhishek Rungta <52626190+abhi10010@users.noreply.github.com>
2020-06-11 11:43:22 +0530

    Create ReverseInteger.py

---
+++ Python/ReverseInteger.py
@@ -0,0 +1,15 @@
+class Solution:
+    def reverse(self, x: int) -> int:
+        s = str(x).strip()
+        if s[0] == '-':
+            x = int(s[0] + s[::-1].replace('-', ''))
+            if x < (-2 ** 31) :
+                return 0
+            else:
+                return x
+        else:
+            x = int(s[::-1])
+            if x > (2 ** 31) - 1:
+                return 0
+            else:
+                return x
