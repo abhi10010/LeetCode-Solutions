f131ae9f417ada16cd91d3c93409a0e7557304f1
Abhishek Rungta <52626190+abhi10010@users.noreply.github.com>
2020-06-12 13:57:23 +0530

    Update ShuffleTheArray.py

--- Python/ShuffleTheArray.py
+++ Python/ShuffleTheArray.py
@@ -1,10 +1,13 @@
 class Solution:
     def shuffle(self, nums: List[int], n: int) -> List[int]:
+        
         res = [0]*n*2
         i,j = 0, 0
+        
         while i < n and j < n*2:
             res[j] = nums[i]
             res[j+1] = nums[i+n]
             i+=1
             j+=2
+        
         return res
