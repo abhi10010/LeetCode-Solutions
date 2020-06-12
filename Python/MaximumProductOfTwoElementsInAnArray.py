2de6137be7b1d19e1fbd3e67b5a25e9c3ba68b57
Abhishek Rungta <52626190+abhi10010@users.noreply.github.com>
2020-06-12 13:53:32 +0530

    Update MaximumProductOfTwoElementsInAnArray.py

--- Python/MaximumProductOfTwoElementsInAnArray.py
+++ Python/MaximumProductOfTwoElementsInAnArray.py
@@ -1,9 +1,12 @@
 class Solution:
     def maxProduct(self, nums: List[int]) -> int:
+        
         max_val = max(nums)
         nums.remove(max_val)
         new = nums.copy()
+        
         for i in range(len(nums)):
             nums[i] = nums[i] - max_val
+        
         max_val_2 = new[nums.index(max(nums))]
         return (max_val-1) * (max_val_2-1)
