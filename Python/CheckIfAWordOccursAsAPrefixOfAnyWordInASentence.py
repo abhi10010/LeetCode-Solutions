3d2062c93be7e4b07b604ad31a1d91b021f42c3e
Abhishek Rungta <52626190+abhi10010@users.noreply.github.com>
2020-06-12 13:56:32 +0530

    Create CheckIfAWordOccursAsAPrefixOfAnyWordInASentence.py

---
+++ Python/CheckIfAWordOccursAsAPrefixOfAnyWordInASentence.py
@@ -0,0 +1,6 @@
+class Solution:
+    def isPrefixOfWord(self, sentence: str, searchWord: str) -> int:
+        
+        for i in range(len(sentence.split(' '))):
+            if searchWord in sentence.split(' ')[i] and searchWord[0] == sentence.split(' ')[i][0]:
+                return i+1
