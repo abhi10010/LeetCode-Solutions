5ed6f80a83e9e53dbacac05cbb130390d58251bc
Abhishek Rungta <52626190+abhi10010@users.noreply.github.com>
2020-06-12 13:57:56 +0530

    Update RomanToInteger.py

--- Python/RomanToInteger.py
+++ Python/RomanToInteger.py
@@ -1,8 +1,10 @@
 class Solution:
     def romanToInt(self, s: str) -> int:
+        
         val = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
         y = 0
         i = len(s) - 1
+        
         while i>-1:
             if s[i] == 'V' and s[i-1] == 'I' and i!=0:
                 y += 4
@@ -25,4 +27,5 @@ class Solution:
             else:
                 y += val[s[i]]
                 i -= 1
+        
         return y
