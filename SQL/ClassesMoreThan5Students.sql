5cb92c42744f94889f31efb0776f0e1d8ea21079
Abhishek Rungta <52626190+abhi10010@users.noreply.github.com>
2020-06-12 15:45:21 +0530

    Update ClassesMoreThan5Students.sql

--- SQL/ClassesMoreThan5Students.sql
+++ SQL/ClassesMoreThan5Students.sql
@@ -1,10 +1,8 @@
-SELECT 
-    class 
-FROM 
-    (SELECT
-        class, COUNT(DISTINCT student) AS num
-    FROM
-        courses
-    GROUP BY class) AS new
-WHERE
-    num >= 5
+SELECT
+    class
+FROM
+    courses
+GROUP BY 
+    class
+HAVING 
+    COUNT(DISTINCT student) >= 5
