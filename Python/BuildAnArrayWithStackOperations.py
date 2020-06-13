cf031d0eb8ddb920684a920d339783cd84ed1336
Abhishek Rungta <52626190+abhi10010@users.noreply.github.com>
2020-06-13 14:36:39 +0530

    Update BuildAnArrayWithStackOperations.py

--- Python/BuildAnArrayWithStackOperations.py
+++ Python/BuildAnArrayWithStackOperations.py
@@ -1,1 +1,17 @@
-
+class Solution:
+    def buildArray(self, target: List[int], n: int) -> List[str]:
+        
+        res = list()
+        m = max(target)
+        
+        for i in range(n):
+            
+            if i + 1 in target and i + 1 <= m:
+                res.append('Push')
+            elif i + 1 not in target and i + 1 <= m:
+                res.append('Push')
+                res.append('Pop')
+            elif i + 1 > m:
+                continue
+        
+        return res
