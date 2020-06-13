c4e0fd14ca3004125cb0df3a5f985ea860fe0308
Abhishek Rungta <52626190+abhi10010@users.noreply.github.com>
2020-06-13 16:19:15 +0530

    Create ExchangeSeats.sql

---
+++ SQL/ExchangeSeats.sql
@@ -0,0 +1,18 @@
+SELECT
+    (CASE 
+        WHEN((SELECT 
+                MAX(id) 
+              FROM 
+                seat)%2 = 1) 
+        AND id = 
+            (SELECT 
+                MAX(id) 
+             FROM 
+                seat) 
+        THEN id
+        WHEN id % 2 = 1 
+        THEN id + 1
+        ELSE id - 1 
+      END) AS id, student
+FROM seat
+ORDER BY id
