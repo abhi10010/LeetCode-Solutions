794d5f162a216967c89d342c8e02457661bc5a33
Abhishek Rungta <52626190+abhi10010@users.noreply.github.com>
2020-06-14 13:09:30 +0530

    Create JewelsAndStones.py

---
+++ Python/JewelsAndStones.py
@@ -0,0 +1,9 @@
+class Solution:
+    def numJewelsInStones(self, J: str, S: str) -> int:
+        
+        count = 0
+        
+        for i in J:
+            count += S.count(i)
+        
+        return count
