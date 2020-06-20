6c064c20afdba648945120806d7c85631b22e3c4
Abhishek Rungta <52626190+abhi10010@users.noreply.github.com>
2020-06-21 01:18:34 +0530

    Create N-RepeatedElementInSize2NArray.py

---
+++ Python/N-RepeatedElementInSize2NArray.py
@@ -0,0 +1,8 @@
+class Solution:
+    def repeatedNTimes(self, A: List[int]) -> int:
+        
+        for i in A:
+            
+            if A.count(i) > 1:
+                
+                return i
