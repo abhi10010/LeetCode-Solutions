38f6ec85b8466c35250e51c6d3cf77a3106b0edc
Abhishek Rungta <52626190+abhi10010@users.noreply.github.com>
2020-06-25 00:45:52 +0530

    Create UniqueNumberOfOccurrences.py

---
+++ Python/UniqueNumberOfOccurrences.py
@@ -0,0 +1,3 @@
+class Solution:
+    def uniqueOccurrences(self, arr: List[int]) -> bool:
+        return len(set([y for x,y in Counter(arr).most_common()]))==len([y for x,y in Counter(arr).most_common()])
