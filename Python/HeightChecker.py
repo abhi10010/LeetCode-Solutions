ba27cbbf8b2727a85d025df83149aa2f1daad7ed
Abhishek Rungta <52626190+abhi10010@users.noreply.github.com>
2020-06-25 00:47:50 +0530

    Update HeightChecker.py

--- Python/HeightChecker.py
+++ Python/HeightChecker.py
@@ -1,9 +1,11 @@
 class Solution:
     def heightChecker(self, heights: List[int]) -> int:
+        
         res = 0
         sh = sorted(heights)
+        
         for i in range(len(heights)):
             if heights[i] != sh[i]:
-                res+=1
+                res += 1
         
         return res
